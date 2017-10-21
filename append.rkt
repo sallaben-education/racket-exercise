b081130906724994444c2894d47953e36cbbf7b8
sallaben <steven.allaben@gmail.com>
2017-10-21 13:43:29 -0400

    list append
    As my first venture into functional programming, I chose to write a basic function that appends two lists.

---
+++ append.rkt
@@ -0,0 +1,10 @@
+#lang racket
+
+;list append
+(define (append list1 list2)
+  (cond
+    [(empty? (rest list1)) (cons (first list1) list2)]
+    [else (cons (first list1) (append (rest list1) list2))]))
+
+(displayln "append '(1 2) and '(3 4 5)")
+(append (list 1 2) (list 3 4 5))
