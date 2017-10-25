cde70f702306524582c65d1f92f8cc12e92e3768
sallaben <steven.allaben@gmail.com>
2017-10-25 11:52:02 -0400

    set membership, folder structure

---
+++ set/member.rkt
@@ -0,0 +1,16 @@
+#lang racket
+
+(define (member obj1 set1)
+  (cond
+    [(empty? set1) #f]
+    [(equal? obj1 (first set1)) #t]
+    [else (member obj1 (rest set1))]))
+
+(displayln "member 3 '(1 2 3 4 5)")
+(member 3 (list 1 2 3 4 5))
+
+(displayln "member 6 '(1 2 3)")
+(member 6 (list 1 2 3))
+
+(displayln "member 1 '()")
+(member 6 empty)
