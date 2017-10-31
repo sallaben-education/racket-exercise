03a6d620df63668871a7ec14e007e4ccc4966fc9
sallaben <steven.allaben@gmail.com>
2017-10-31 17:14:53 -0400

    minor formatting

--- list/append.rkt
+++ list/append.rkt
@@ -7,5 +7,5 @@
     [(empty? (rest list1)) (cons (first list1) list2)]
     [else (cons (first list1) (append (rest list1) list2))]))
 
-(displayln "append '(1 2) and '(3 4 5)")
+(displayln "append '(1 2) '(3 4 5)")
 (append (list 1 2) (list 3 4 5))
